6cd5867f2ed561b78237450a92a285efedcc7182
lixu <wflixu@88.com>
2022-02-26 22:28:01 +0800

    docs

---
+++ script/docs.mjs
@@ -0,0 +1,35 @@
+#!/usr/bin/env zx
+
+
+
+
+
+// await $`cat package.json | grep name`
+
+// let branch = await $`git branch --show-current`
+
+
+// await Promise.all([
+//   $`sleep 1; echo 1`,
+//   $`sleep 2; echo 2`,
+//   $`sleep 3; echo 3`,
+// ])
+
+// let name = 'foo bar'
+// await $`mv /tmp/${name}`
+
+// await $`cat package.json`.pipe(process.stdout);
+
+
+// let resp = await fetch('http://wttr.in')
+// if (resp.ok) {
+//   console.log(await resp.text())
+// }
+
+
+// let bear = await question('What kind of bear is best? ')
+// let token = await question('Choose env variable: ', {
+//   choices: Object.keys(process.env)
+// })
+
+
