d83788abb906ae4f196a4a0ee296b4af8e439891
wflixu <wflixu@88.com>
2024-10-10 16:34:28 +0800

    build(sourcemap):dev mode add sourcemap output

--- packages/thgraph/rollup.config.js
+++ packages/thgraph/rollup.config.js
@@ -1,9 +1,10 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 import image from '@rollup/plugin-image';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,6 +32,7 @@ const bundles = [
     input,
     output: {
       file: path.join(__dirname, 'dist/thgraph.esm.development.js'),
+      sourcemap: true,
       format: 'esm',
     },
     plugins: []
@@ -61,44 +63,26 @@ const isMinEnv = (file) => file.includes('.min.');
 const isSpecificEnv = (file) => isMinEnv(file) || isDevEnv(file);
 const isDebugAlways = (file) => isDevEnv(file) || isUMD(file) ? 'true' : 'false';
 
-const buildExport = bundles.map(({input, output, plugins}) => ({
+const buildExport = bundles.map(({ input, output, plugins }) => ({
   input,
   output,
   external: ['detect-browser'],
   plugins: [
-    
     image(),
-    nodeResolve({extensions: ['.js']}),
+    nodeResolve({ extensions: ['.js'] }),
     replace({
       __DEV__: isSpecificEnv(output.file)
         ? isDebugAlways(output.file)
         : 'process.env.NODE_ENV !== "production"',
       preventAssignment: true,
     }),
-    
+
     ...plugins,
-    
+
     output.file.includes('.min.') && terser(),
   ],
 }));
 
-const devExport = {
-  input: path.join(__dirname, 'src/index.ts'),
-  output: {
-    file: path.join(__dirname, `dist/thgraph.esm.js`),
-    format: 'esm',
-  },
-  external: ['detect-browser'],
-  plugins: [
-    image(),
-    nodeResolve({extensions: ['.js']}),
-   
-   
-    replace({
-      __DEV__: 'true',
-      preventAssignment: true,
-    }),
-  ],
-};
 
-export default process.env.NODE_ENV === 'build' ? buildExport : devExport;
+
+export default buildExport;
